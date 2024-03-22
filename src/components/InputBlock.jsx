af655062da4ef28a310d7ac870b63ad669c46c9c
Roman Meszaros <roman-meszaros@lxuser-HP-ProBook-450-15-6-inch-G10-Notebook-PC>
2024-03-22 10:54:03 +0100

    authorization: changed type for password input field
    authorization: changed type for password input field

--- src/components/InputBlock.jsx
+++ src/components/InputBlock.jsx
@@ -3,11 +3,16 @@ import { PropTypes } from 'prop-types';
 
 export function InputBlock({ children, register, error }) {
   const name = children.split(' ').join('');
+  const isPassword = children.split(' ')[0] === 'password' ? true : false;
 
   return (
     <div>
       <Label htmlFor={name}>{children}</Label>
-      {register ? <Input id={name} {...register(name)} /> : <Input id={name} />}
+      {register ? (
+        <Input id={name} {...register(name)} type={isPassword ? 'password' : 'text'} />
+      ) : (
+        <Input id={name} type={isPassword ? 'password' : 'text'} />
+      )}
       <ErrorMessage>{error}</ErrorMessage>
     </div>
   );
