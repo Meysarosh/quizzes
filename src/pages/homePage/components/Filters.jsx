a0f26aa93cd22108b9f9a39232024309967b49a5
Roman Meszaros <roman-meszaros@lxuser-HP-ProBook-450-15-6-inch-G10-Notebook-PC>
2024-04-02 15:23:33 +0200

    logic: implemented filtering logic

--- src/pages/homePage/components/Filters.jsx
+++ src/pages/homePage/components/Filters.jsx
@@ -1,18 +1,131 @@
 import { PropTypes } from 'prop-types';
 import { Title, Header, ButtonClose } from './Filters.styles';
-import { FormField } from '../../../components/formField';
-
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  setQuizBankFilter,
+  setDifficultyFilterAll,
+  setDifficultyFilter,
+  filterByDifficulty,
+  filterByQuizBank,
+  setQuantityFilter,
+} from '../../../store/slices/questionsSlice';
+import { useEffect, useState } from 'react';
 export function Filters({ onClick }) {
+  const dispatch = useDispatch();
+  const { filters } = useSelector((state) => state.questions);
+  const { quizBanks } = useSelector((state) => state.questions);
+  const { difficulties } = useSelector((state) => state.questions);
+  const { questions } = useSelector((state) => state.questions);
+  const { filteredQuestions } = useSelector((state) => state.questions);
+
+  const [all, setAll] = useState(true);
+  const [difficultiesForFilter, setDifficultiesForFilter] = useState([]);
+  const [questionsCount, setQuestionsCount] = useState(questions.length);
+
+  useEffect(() => {
+    setDifficultiesForFilter(
+      filters.quizBank === 'React' ? difficulties.React : difficulties.Other
+    );
+  }, [filters.quizBank, difficulties]);
+
+  useEffect(() => {
+    if (filters.difficulty.length === difficultiesForFilter.length) setAll(true);
+    else setAll(false);
+  }, [filters.difficulty, difficultiesForFilter.length]);
+
+  useEffect(() => {
+    dispatch(setDifficultyFilterAll(difficultiesForFilter));
+    setAll(true);
+  }, [filters.quizBank, difficultiesForFilter, dispatch]);
+
+  useEffect(() => {
+    setQuestionsCount(filteredQuestions.length);
+  }, [filteredQuestions]);
+
+  function handleBankChange(e) {
+    dispatch(setQuizBankFilter(e.target.value));
+    dispatch(filterByQuizBank());
+  }
+
+  function handleCheckboxAllChange() {
+    if (all) {
+      dispatch(setDifficultyFilterAll([]));
+      setAll(false);
+    } else {
+      dispatch(setDifficultyFilterAll(difficultiesForFilter));
+      setAll(true);
+    }
+    dispatch(filterByDifficulty());
+  }
+
+  function handleCheckboxChange(e) {
+    dispatch(setDifficultyFilter(e.target.name));
+    dispatch(filterByDifficulty());
+  }
+
+  function handleQuantityChange(e) {
+    dispatch(setQuantityFilter(e.target.value));
+  }
+
+  function isChecked(name) {
+    const result = filters.difficulty.find((el) => el === name);
+    if (result) return true;
+    return false;
+  }
+
   return (
     <>
       <Header>
         <Title>Filters</Title>
         <ButtonClose onClick={onClick}></ButtonClose>
       </Header>
-      <FormField>Name</FormField>
-      <FormField>Difficulty</FormField>
-      <FormField>Quiz bank</FormField>
-      <FormField>Status</FormField>
+      <div>
+        <label htmlFor="bank">Quiz bank</label>
+        <select name="bank" id="bank" onChange={handleBankChange}>
+          <option value="">Select</option>
+          {quizBanks.map((bank) => (
+            <option key={bank} value={bank}>
+              {bank}
+            </option>
+          ))}
+        </select>
+      </div>
+      <div>
+        <label>Difficulty</label>
+        <input
+          type="checkbox"
+          id="all"
+          name="all"
+          checked={all}
+          onChange={handleCheckboxAllChange}
+          disabled={filters.quizBank === null ? true : false}
+        />
+        <label htmlFor="all">All</label>
+        {filters.quizBank != null &&
+          difficultiesForFilter.map((difficulty) => (
+            <div key={difficulty}>
+              <input
+                type="checkbox"
+                id={difficulty}
+                name={difficulty}
+                checked={isChecked(difficulty)}
+                onChange={handleCheckboxChange}
+                disabled={filters.quizBank === null ? true : false}
+              />
+              <label htmlFor={difficulty}>{difficulty}</label>
+            </div>
+          ))}
+      </div>
+
+      <p>Available questions: {questionsCount}</p>
+      <div>
+        <label htmlFor="quantity">Questions quantity:</label>
+        <select name="quantity" id="quantity" onChange={handleQuantityChange}>
+          <option value="">All</option>
+          <option value={5}>5</option>
+          <option value={10}>10</option>
+        </select>
+      </div>
     </>
   );
 }
--- src/store/slices/questionsSlice.js
+++ src/store/slices/questionsSlice.js
@@ -1,42 +1,94 @@
 import { createSlice } from '@reduxjs/toolkit';
 import { getQuestions } from '../actions';
 
-const initialState = { questions: {} };
+const initialState = {
+  questions: [],
+  filteredQuestions: [],
+  quizBanks: [],
+  difficulties: {
+    React: ['Beginner', 'Easy', 'Intermediate', 'Advanced'],
+    Other: ['Easy', 'Medium', 'Hard'],
+  },
+  filters: { quizBank: null, difficulty: [], quantity: null },
+};
 
 export const questionsSlice = createSlice({
   name: 'questions',
   initialState,
-  reducers: {},
+  reducers: {
+    setQuizBankFilter(state, action) {
+      if (action.payload === '') {
+        state.filters.quizBank = null;
+        state.filters.difficulty = [];
+      } else {
+        state.filters.quizBank = action.payload;
+      }
+    },
+    filterByQuizBank(state) {
+      if (state.filters.quizBank === null) state.filteredQuestions = state.questions;
+      else
+        state.filteredQuestions = state.questions.filter(
+          (question) => question.title === state.filters.quizBank
+        );
+    },
+    setDifficultyFilterAll(state, action) {
+      state.filters.difficulty = action.payload;
+    },
+    setDifficultyFilter(state, action) {
+      const idx = state.filters.difficulty.indexOf(action.payload);
+      if (idx < 0) state.filters.difficulty.push(action.payload);
+      else state.filters.difficulty.splice(idx, 1);
+    },
+    filterByDifficulty(state) {
+      if (state.filters.quizBank === null) state.filteredQuestions = state.questions;
+      else {
+        const filteredByBank = state.questions.filter(
+          (question) => question.title === state.filters.quizBank
+        );
+        state.filteredQuestions = filteredByBank.filter((question) =>
+          state.filters.difficulty.includes(question.level)
+        );
+      }
+    },
+    setQuantityFilter(state, action) {
+      if (action.payload === '') state.filters.quantity = null;
+      else state.filters.quantity = action.payload;
+    },
+  },
   extraReducers: (builder) => {
     builder.addCase(getQuestions.fulfilled, (state, action) => {
       state.questions = processData(action.payload);
+      state.quizBanks = readQuizBanks(action.payload);
+      state.filteredQuestions = processData(action.payload);
     });
   },
 });
 
 function processData(data) {
-  const topics = [];
-
+  const questions = [];
   const entries = Object.entries(data);
 
   entries.forEach((entry) => {
-    const curTopicsSet = new Set();
     const title = entry[0];
     entry[1].forEach((question) => {
-      curTopicsSet.add(question.topic);
-    });
-
-    curTopicsSet.forEach((topic) => {
-      const topicQuestions = entry[1].filter((question) => question.topic === topic);
-      topics.push({
-        title,
-        topic,
-        questions: topicQuestions,
-      });
+      questions.push({ ...question, title: title });
     });
   });
 
-  return { topics };
+  return questions;
+}
+
+function readQuizBanks(data) {
+  const quizBanks = Object.keys(data);
+  return quizBanks;
 }
 
 export default questionsSlice.reducer;
+export const {
+  setQuizBankFilter,
+  filterByQuizBank,
+  setDifficultyFilterAll,
+  setDifficultyFilter,
+  filterByDifficulty,
+  setQuantityFilter,
+} = questionsSlice.actions;
